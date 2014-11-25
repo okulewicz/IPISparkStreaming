04e386875ed1fbac9f918e2c98fbcf1c0796070b
Michał Okulewicz <okulewicz.michal@gmail.com>
2014-11-26 00:55:14 +0100

    Literature and synchronization of generated numbers

--- StreamDataGenerator/src/pl/waw/ipipan/phd/okulewicz/DataGenerator.java
+++ StreamDataGenerator/src/pl/waw/ipipan/phd/okulewicz/DataGenerator.java
@@ -4,18 +4,24 @@ import java.util.Random;
 
 public class DataGenerator implements Runnable {
 
-	public static int number = 0; 
+	public static long number = 0; 
 	final public static Object lockObj = new Object();
 	static Random random = new Random();
 	
 	public static byte[] getData() {
-		return (Integer.toString(number) + "\n").getBytes();
+		long number = 0;
+		synchronized(lockObj) {
+			number = DataGenerator.number;
+		}
+		return (Long.toString(number) + "\n").getBytes();
 	}
 
 	@Override
 	public void run() {
 		while (true) {
+			synchronized (lockObj) {
 				++number;
+			}
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
